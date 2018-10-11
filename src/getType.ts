336e14d608e4ccb3fcf24940a50cd2784b475c4d
Joy Fong <fjc0kb@gmail.com>
2018-10-11 17:03:47 +0800

    chore: tweaks

--- src/getType.ts
+++ src/getType.ts
@@ -6,5 +6,5 @@
  * @returns 检测值的类型
  */
 export default function getType(value: any): string {
-  return Object.prototype.toString.call(value).split(' ')[1].replace(']', '')
+  return Object.prototype.toString.call(value).slice(8, -1)
 }
