3ce1aad63e29a93d42a837f5e609cf60d2806fea
Joy Fong <fjc0kb@gmail.com>
2018-10-11 16:30:22 +0800

    chore: tweaks

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,3 +1,15 @@
+function reduce<T, K extends number, R>(
+  data: ReadonlyArray<T>,
+  fn: (accumulator: R, currentValue: T, currentKey: K) => R,
+  accumulator: R
+): R
+
+function reduce<T extends object, K extends keyof T, R>(
+  data: T,
+  fn: (accumulator: R, currentValue: T[K], currentKey: K) => R,
+  accumulator: R
+): R
+
 /**
  * 将一个数组或一个对象归纳为一个值。
  *
@@ -6,16 +18,6 @@
  * @param initialValue 归纳初始值
  * @returns 归纳最终值
  */
-function reduce<T, K extends number, R>(
-  data: ReadonlyArray<T>,
-  fn: (accumulator: R, currentValue: T, currentKey: K) => R,
-  accumulator: R
-): R
-function reduce<T extends object, K extends keyof T, R>(
-  data: T,
-  fn: (accumulator: R, currentValue: T[K], currentKey: K) => R,
-  accumulator: R
-): R
 function reduce(data: any, fn: any, accumulator: any): any {
   if (Array.isArray(data)) {
     return data.reduce(fn, accumulator)
