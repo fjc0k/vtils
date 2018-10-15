3484ed6b5f0885cae8a403cdf8f03264ed5ec44d
Joy Fong <fjc0kb@gmail.com>
2018-10-15 09:26:44 +0800

    fix: types

--- src/cssTransform.ts
+++ src/cssTransform.ts
@@ -1,3 +1,13 @@
+interface CssTransform {
+  (el: HTMLElement, transformRule: string, transitionRule: string): void,
+  /**
+   * 停止 CSS 变换。
+   *
+   * @param el 要停止变换的元素
+   */
+  stop(el: HTMLElement): void
+}
+
 /**
  * CSS 变换。
  *
@@ -5,23 +15,16 @@
  * @param transformRule 变换规则
  * @param transitionRule 过渡规则
  */
-function cssTransform(el: HTMLElement, transformRule: string, transitionRule: string): void {
+const cssTransform: CssTransform = (el: HTMLElement, transformRule: string, transitionRule: string): void => {
   el.style.webkitTransform = transformRule
   el.style.transform = transformRule
   el.style.webkitTransition = `-webkit-transform ${transitionRule}`
   el.style.transition = `transform ${transitionRule}`
 }
 
-/**
- * 停止 CSS 变换。
- *
- * @param el 要停止变换的元素
- */
-function stop(el: HTMLElement): void {
+cssTransform.stop = (el: HTMLElement): void => {
   el.style.webkitTransition = 'none'
   el.style.transition = 'none'
 }
-
-cssTransform.stop = stop
 
 export default cssTransform
