4db9f7a51832df6fb72f1826a548639bf953d0c4
Joy Fong <fjc0kb@gmail.com>
2018-10-15 10:12:33 +0800

    chore(release): 0.14.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="0.14.4"></a>
+## [0.14.4](https://github.com/fjc0k/vtils/compare/v0.14.3...v0.14.4) (2018-10-15)
+
+
+
 <a name="0.14.3"></a>
 ## [0.14.3](https://github.com/fjc0k/vtils/compare/v0.14.2...v0.14.3) (2018-10-15)
 
--- lib/bindEvent.d.ts
+++ lib/bindEvent.d.ts
@@ -1,6 +1,6 @@
 export declare type BindEventTypes = string | string[];
 export declare type BindEventListener = EventListenerOrEventListenerObject;
-export declare type BindEventOptions = boolean | AddEventListenerOptions;
+export declare type BindEventListenerOptions = boolean | AddEventListenerOptions;
 export declare type UnbindEventListener = () => void;
 /**
  * 将指定类型的事件绑定在指定的目标上并返回解绑函数。
@@ -10,4 +10,4 @@ export declare type UnbindEventListener = () => void;
  * @param listener 事件监听器
  * @param [options] 事件选项
  */
-export default function bindEvent(target: EventTarget, types: BindEventTypes, listener: EventListenerOrEventListenerObject, options?: BindEventOptions): UnbindEventListener;
+export default function bindEvent(target: EventTarget, types: BindEventTypes, listener: EventListenerOrEventListenerObject, options?: BindEventListenerOptions): UnbindEventListener;
