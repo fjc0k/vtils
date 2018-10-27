77673d004368b2637acb34d6c00a19455b82f314
fjc0k <fjc0kb@gmail.com>
2018-10-27 22:54:39 +0800

    chore: tweaks

--- src/getType.ts
+++ src/getType.ts
@@ -13,6 +13,7 @@ export type ValueType = (
  * @param value 要检测的值
  * @returns 检测值的类型
  * @see https://www.ecma-international.org/ecma-262/6.0/#sec-object.prototype.tostring
+ * @see https://www.ecma-international.org/ecma-262/5.1/#sec-8.6.2
  */
 export default function getType(value: any): ValueType {
   return Object.prototype.toString.call(value).slice(8, -1)
