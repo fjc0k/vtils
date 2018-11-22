3aafeded6dcb6a62db4c9e9ce281609aeaf68aac
Jay Fong <fjc0kb@gmail.com>
2018-11-22 11:32:43 +0800

    feat: add isHan, isChineseName

---
+++ src/isChineseName.ts
@@ -0,0 +1,18 @@
+import isHan from './isHan'
+
+/**
+ * 检测 `value` 是否是中国人姓名，支持少数名族姓名中间的 `·` 号。
+ *
+ * @param value 要检测的值
+ * @returns 是（true）或否（false）
+ */
+export default function isChineseName(value: string): boolean {
+  return (
+    value
+      && value.length > 1
+      && value.length < 20
+      && value[0] !== '\u00B7'
+      && value.indexOf('\u00B7\u00B7') === -1
+      && isHan(value.replace(/\u00B7/g, ''))
+  )
+}
