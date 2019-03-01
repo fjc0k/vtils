8a5a56ced6883a940e5445ce0d9c0cc613e50bee
Jay Fong <fjc0kb@gmail.com>
2019-03-01 13:32:22 +0800

    feat(keyBy): 仅支持函数生成 key

--- src/keyBy.ts
+++ src/keyBy.ts
@@ -1,24 +1,20 @@
-import get from './get'
-import { ToPathValue } from './toPath'
-
 /**
  * 根据 `iteratee` 对 `arr` 进行分组，但只保留最后一个结果。
  *
  * @param arr 要分组的数据
- * @param iteratee 迭代值，字符串或数字表示键路径，函数表示以该函数生成 `key`
+ * @param iteratee 生成 `key` 的迭代函数
  * @returns 分组结果
  */
-export default function keyBy<T>(
+export default function keyBy<T extends any, K extends keyof any>(
   arr: T[],
-  iteratee: ToPathValue | ((item: T, index: number) => any)
-): { [key: string]: T } {
-  const iterateeIsFunction = typeof iteratee === 'function'
-  return arr.reduce<{ [key: string]: T }>((res, item, index) => {
-    res[
-      iterateeIsFunction
-        ? (iteratee as any)(item, index)
-        : get(item as any, iteratee as any)
-    ] = item
-    return res
-  }, {})
+  iteratee: (item: T, index: number) => K,
+): { [key in K]: T } {
+  return arr.reduce<{ [key in K]: T }>(
+    (res, item, index) => {
+      const key = iteratee(item, index)
+      res[key] = item
+      return res
+    },
+    {} as any,
+  )
 }
