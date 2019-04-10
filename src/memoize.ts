9da18a980384e04f8c2eddf8ed3e59c3df32384f
Jay Fong <fjc0kb@gmail.com>
2019-04-11 00:16:29 +0800

    feat: rewrite memoize

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,3 +1,34 @@
-import memoize from 'fast-memoize'
+import fastMemoize, { Options } from 'fast-memoize'
+import { AnyFunction } from './isFunction'
 
-export { memoize }
+export interface MemoizeOptions<T extends AnyFunction = AnyFunction> {
+  createCache?(): {
+    set(key: string, value: any): void,
+    get(key: string): any,
+    has(key: string): boolean,
+  },
+  serializer?(args: Parameters<T>): string,
+}
+
+/**
+ * 函数参数结果缓存。
+ *
+ * @param fn 要缓存的函数
+ * @param [options] 选项
+ * @returns 缓存化后的函数
+ */
+export function memoize<T extends AnyFunction>(fn: T, options?: MemoizeOptions<T>): T {
+  let fastMemoizeOptions: Options<T> = undefined
+  if (options) {
+    fastMemoizeOptions = {}
+    if (options.createCache) {
+      fastMemoizeOptions.cache = {
+        create: options.createCache,
+      } as any
+    }
+    if (options.serializer) {
+      fastMemoizeOptions.serializer = options.serializer as any
+    }
+  }
+  return fastMemoize(fn, fastMemoizeOptions)
+}
