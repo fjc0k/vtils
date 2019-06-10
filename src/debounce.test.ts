2148d25a5c6345726813b92d6c39063f6598a670
Jay Fong <fjc0kb@gmail.com>
2019-06-10 16:02:56 +0800

    feat: 新增 debounce 和 throttle

---
+++ src/debounce.test.ts
@@ -0,0 +1,21 @@
+import { debounce } from './debounce'
+import { times } from './times'
+import { wait } from './wait'
+
+test('表现正常', async () => {
+  const fn: (x: number, y: number) => number = jest.fn().mockImplementation((x: number, y: number) => x + y)
+
+  const debouncedFn = debounce(fn, 500)
+
+  times(10000, () => debouncedFn(1, 2))
+
+  await wait(550)
+
+  expect(fn).toBeCalledTimes(1)
+
+  times(10000, () => debouncedFn(1, 2))
+
+  await wait(550)
+
+  expect(fn).toBeCalledTimes(2)
+})
---
+++ src/debounce.ts
@@ -0,0 +1,36 @@
+import { AnyFunction } from './enhanceType'
+
+/**
+ * 创建一个去抖函数，将触发频繁的事件合并成一次执行。
+ *
+ * 该函数被调用后，计时 `wait` 毫秒后调用 `fn` 函数。
+ * 若在 `wait` 毫秒内该函数再次被调用，则重新开始计时。
+ *
+ * 一个应用场景：监听输入框的 `input` 事件发起网络请求。
+ *
+ * @param fn 要去抖的函数
+ * @param wait 需要等待的毫秒数
+ * @returns 返回去抖后的函数
+ * @example
+ * ```ts
+ * document.querySelector('#input').oninput = debounce(
+ *   e => {
+ *     console.log(e.target.value)
+ *   },
+ *   500,
+ * )
+ * ```
+ */
+export function debounce<T extends AnyFunction>(fn: T, wait: number): (...args: Parameters<T>) => void {
+  let timer: NodeJS.Timeout | null = null
+
+  return function (this: any) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(
+      () => fn.apply(this, arguments as any),
+      wait,
+    )
+  }
+}
