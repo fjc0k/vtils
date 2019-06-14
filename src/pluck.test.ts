14c524e9a37f09aa1ddd9c90d8b47ff0f317fe4c
Jay Fong <fjc0kb@gmail.com>
2019-06-14 10:00:20 +0800

    feat: 新增 pluck

---
+++ src/pluck.test.ts
@@ -0,0 +1,16 @@
+import { jestExpectEqual } from './enhanceJest'
+import { pluck } from './pluck'
+
+test('提取为数组正常', () => {
+  jestExpectEqual(
+    pluck([{ x: 1, y: 'z' }, { y: 2, x: 5 }], item => item.x),
+    [1, 5],
+  )
+})
+
+test('提取为对象正常', () => {
+  jestExpectEqual(
+    pluck([{ x: 1, y: 'z' }, { y: 2, x: 5 }], item => item.x, item => item.y),
+    { z: 1, 2: 5 },
+  )
+})
