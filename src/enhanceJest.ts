27f3c2c1ff647521881a5669abc1a268d04f588c
Jay Fong <fjc0kb@gmail.com>
2019-06-17 10:55:28 +0800

    docs(jestExpectEqual): 添加文档

--- src/enhanceJest.ts
+++ src/enhanceJest.ts
@@ -1,3 +1,9 @@
-export function jestExpectEqual<T>(a: T, b: T) {
-  return expect(a).toEqual(b)
+/**
+ * 这是一个 jest 测试辅助函数，等同于 `expect(actual).toEqual(expected)`，只不过是加上了类型。
+ *
+ * @param actual 测试值
+ * @param expected 期望值
+ */
+export function jestExpectEqual<T>(actual: T, expected: T) {
+  return expect(actual).toEqual(expected)
 }
