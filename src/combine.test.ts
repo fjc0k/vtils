638b68572d5c2b2f8a64e38febb6135b333a820e
Jay Fong <fjc0kb@gmail.com>
2019-07-18 14:16:11 +0800

    feat: 新增 combine

---
+++ src/combine.test.ts
@@ -0,0 +1,26 @@
+import { combine } from './combine'
+import { jestExpectEqual } from './enhanceJest'
+
+test('表现正常', () => {
+  const keys = [1, 2, 'hello']
+  const values = [false, [], { q: 'w' }]
+
+  jestExpectEqual(
+    combine(keys, values),
+    {
+      1: false,
+      2: [],
+      hello: { q: 'w' },
+    },
+  )
+
+  jestExpectEqual(
+    combine([...keys, 'no'], values),
+    {
+      1: false,
+      2: [],
+      hello: { q: 'w' },
+      no: undefined as any,
+    },
+  )
+})
