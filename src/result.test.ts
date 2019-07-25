480685493375249b3f96def814456205bab16f0b
Jay Fong <fjc0kb@gmail.com>
2019-07-25 10:46:56 +0800

    feat: 新增 result

---
+++ src/result.test.ts
@@ -0,0 +1,50 @@
+import { jestExpectEqual } from './enhanceJest'
+import { result } from './result'
+
+test('Promise 正常', async () => {
+  jestExpectEqual(
+    await result(
+      new Promise<number>(resolve => resolve(2)),
+    ),
+    [null, 2],
+  )
+
+  jestExpectEqual(
+    await result(
+      new Promise<number>((resolve, reject) => reject('err')),
+    ),
+    ['err'] as any,
+  )
+})
+
+test('() => Promise 正常', async () => {
+  jestExpectEqual(
+    await result(
+      () => new Promise<number>(resolve => resolve(2)),
+    ),
+    [null, 2],
+  )
+
+  jestExpectEqual(
+    await result(
+      () => new Promise<number>((resolve, reject) => reject('err')),
+    ),
+    ['err'] as any,
+  )
+})
+
+test('() => !Promise 正常', async () => {
+  jestExpectEqual(
+    await result(
+      () => 2,
+    ),
+    [null, 2],
+  )
+
+  jestExpectEqual(
+    await result(
+      () => { throw 'err' },
+    ),
+    ['err'] as any,
+  )
+})
---
+++ src/result.ts
@@ -0,0 +1,47 @@
+import { isPromiseLike } from './is'
+
+/**
+ * 以数组的方式返回异步操作的结果。
+ *
+ * @param action 要执行的异步操作
+ * @returns 返回 `[错误信息, 结果数据]`，错误信息为 `null` 表示没有错误
+ */
+export function result<T extends PromiseLike<any>>(action: T): Promise<[
+  any,
+  T extends PromiseLike<infer R> ? R : any,
+]>
+
+/**
+ * 以数组的方式返回函数执行的结果，如果函数返回一个异步操作，将会执行该异步操作并将其结果作为函数执行的结果返回。
+ *
+ * @param action 要执行的函数
+ * @returns 返回 `[错误信息, 结果数据]`，错误信息为 `null` 表示没有错误
+ */
+export function result<T extends () => any, R extends ReturnType<T>>(action: T): Promise<[
+  any,
+  R extends PromiseLike<any> ? (R extends PromiseLike<infer RR> ? RR : any) : R,
+]>
+
+export function result(action: any): Promise<[any, any]> {
+  return new Promise(resolve => {
+    if (isPromiseLike(action)) {
+      return action.then(
+        res => resolve([null, res]),
+        err => resolve([err] as any),
+      )
+    }
+
+    try {
+      const res = action()
+      if (!isPromiseLike(res)) {
+        return resolve([null, res])
+      }
+      return res.then(
+        res => resolve([null, res]),
+        err => resolve([err] as any),
+      )
+    } catch (err) {
+      return resolve([err] as any)
+    }
+  })
+}
