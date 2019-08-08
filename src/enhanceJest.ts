a0ccf7ec2fd61d5974051bc3df2a8a035632b613
Jay Fong <fjc0kb@gmail.com>
2019-08-08 10:26:30 +0800

    feat(jestExpectEqual): 支持设置多个期望值

--- src/enhanceJest.ts
+++ src/enhanceJest.ts
@@ -2,8 +2,10 @@
  * 这是一个 jest 测试辅助函数，等同于 `expect(actual).toEqual(expected)`，只不过是加上了类型。
  *
  * @param actual 测试值
- * @param expected 期望值
+ * @param expected 期望值列表
  */
-export function jestExpectEqual<T>(actual: T, expected: T) {
-  return expect(actual).toEqual(expected)
+export function jestExpectEqual<T>(actual: T, ...expected: T[]) {
+  expected.forEach(item => {
+    expect(actual).toEqual(item)
+  })
 }
