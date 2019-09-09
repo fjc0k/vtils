a98695cbde52b668c762b2a1ca770964ab1847ba
Jay Fong <fjc0kb@gmail.com>
2019-09-09 11:27:51 +0800

    feat: 新增 remove、removeByValue

---
+++ src/remove.test.ts
@@ -0,0 +1,34 @@
+import { jestExpectEqual } from './enhanceJest'
+import { remove } from './remove'
+
+test('删除正常', () => {
+  const arr = [1, 2, 3]
+
+  jestExpectEqual(
+    remove(arr, 1),
+    [1, 3],
+  )
+})
+
+test('越界正常', () => {
+  const arr = [1, 2, 3]
+
+  jestExpectEqual(
+    remove(arr, 6),
+    [1, 2, 3],
+  )
+
+  jestExpectEqual(
+    remove(arr, -1),
+    [1, 2, 3],
+  )
+})
+
+test('是原地删除', () => {
+  const arr = [1, 2, 3]
+
+  jestExpectEqual(
+    remove(arr, 1) === arr,
+    true,
+  )
+})
