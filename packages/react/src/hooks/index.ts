c357d12e59faf3c7cf2ed0ecea4366c3cf245e61
Jay Fong <fjc0kb@gmail.com>
2019-09-18 21:45:08 +0800

    chore(react): 将 React Hooks 移至 hooks 目录

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,2 +1,3 @@
 // @index(['./*', '!./_*', '!./*.test.ts'], (pp, cc) => `export * from '${pp.path}'`)
-export * from './hooks'
+export * from './useEasyValidator'
+export * from './useLiveEasyValidator'
