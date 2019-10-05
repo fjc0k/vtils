f31610451b232d84c4714acfd7317d214da5d463
Jay Fong <fjc0kb@gmail.com>
2019-10-06 01:51:56 +0800

    feat(date): 新增 numeralDayToChineseDay

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -1,5 +1,6 @@
 // @index(['./*', '!./_*', '!./*.test.ts'], (pp, cc) => `export * from '${pp.path}'`)
 export * from './enhanceDayjs'
 export * from './formatDate'
+export * from './numeralDayToChineseDay'
 export * from './toDayjs'
 // @endindex
---
+++ packages/date/src/numeralDayToChineseDay.ts
@@ -0,0 +1,27 @@
+const dayMap: Record<number, string> = {
+  0: '日',
+  1: '一',
+  2: '二',
+  3: '三',
+  4: '四',
+  5: '五',
+  6: '六',
+  7: '日',
+}
+
+/**
+ * 数字星期转中文星期。`0` 和 `7` 都视为星期日。
+ *
+ * @param day 数字的星期几
+ * @returns 返回中文的星期几
+ * @example
+ * ```ts
+ * numeralDayToChineseDay(0) // => 日
+ * numeralDayToChineseDay(1) // => 一
+ * numeralDayToChineseDay(5) // => 五
+ * numeralDayToChineseDay(7) // => 日
+ * ```
+ */
+export function numeralDayToChineseDay(day: number) {
+  return dayMap[day]
+}
