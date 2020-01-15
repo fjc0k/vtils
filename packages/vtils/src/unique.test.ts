32613ae2e35dd5eab1f7b55214ab954f7cfd4d63
Jay Fong <fjc0kb@gmail.com>
2020-01-15 20:18:53 +0800

    feat: 新增 uniqueBy

--- packages/vtils/src/unique.test.ts
+++ packages/vtils/src/unique.test.ts
@@ -1,5 +1,5 @@
 import {jestExpectEqual} from './enhanceJest'
-import {unique} from './unique'
+import {unique, uniqueBy} from './unique'
 
 test('unique', () => {
   jestExpectEqual(
@@ -10,5 +10,17 @@ test('unique', () => {
   jestExpectEqual(
     unique([1, 2, 1, 3]),
     [1, 2, 3],
+  )
+})
+
+test('uniqueBy', () => {
+  jestExpectEqual(
+    uniqueBy([], item => item < 3),
+    [],
+  )
+
+  jestExpectEqual(
+    uniqueBy([1, 2, 1, 3], item => item < 3),
+    [1, 3],
   )
 })
