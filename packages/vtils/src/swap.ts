42e16149031f156d3e051b48d2746b529222119f
Jay Fong <fjc0kb@gmail.com>
2020-04-30 20:27:15 +0800

    feat: 新增 swap

---
+++ packages/vtils/src/swap.ts
@@ -0,0 +1,19 @@
+/**
+ * 原地交换数组中的两个元素。
+ *
+ * @param arr 要处理的数组
+ * @param i 第一个元素的索引
+ * @param j 第二个元素的索引
+ * @returns 返回交换后的数组
+ * @example
+ * ```ts
+ * swap([1, 2, 3], 0, 2) // => [3, 2, 1]
+ * ```
+ */
+export function swap<T>(arr: T[], i: number, j: number): T[] {
+  const iItem = arr[i]
+  const jItem = arr[j]
+  arr[i] = jItem
+  arr[j] = iItem
+  return arr
+}
