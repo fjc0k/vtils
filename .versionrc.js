4ee5049dba693487ce04003e425c615d2b1a6163
Jay Fong <fjc0kb@gmail.com>
2020-05-29 10:16:37 +0800

    fix: version

--- .versionrc.js
+++ .versionrc.js
@@ -1,6 +1,11 @@
 /** @type import('standard-version').Options */
 module.exports = {
+  // ref: https://github.com/conventional-changelog/standard-version#can-i-use-standard-version-for-additional-metadata-files-languages-or-version-files
   bumpFiles: [
+    {
+      filename: 'package.json',
+      type: 'json',
+    },
     {
       filename: 'mod.ts',
       updater: '.versionrc.mod-updater.js',
