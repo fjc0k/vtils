e7e87c15307717108ed712423c89bc5e657ee43f
Jay Fong <fjc0kb@gmail.com>
2020-06-09 10:08:00 +0800

    up

--- src/date/formatDate.test.ts
+++ src/date/formatDate.test.ts
@@ -29,12 +29,5 @@ describe(formatDate.name, () => {
         _ => `${_.yyyy}年${_.mm}月${_.dd}日${_.hh}时${_.ii}分${_.ss}秒`,
       ),
     ).toBe('2020年05月20日13时45分08秒')
-
-    expect(
-      formatDate(
-        new Date(2020, 5 - 1, 20, 13, 45, 8).getTime(),
-        (_, __) => `${__.b}`,
-      ),
-    ).toBe('下午')
   })
 })
