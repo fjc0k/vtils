1d1c199a6d578e2e1b887ec5303fd0b037110d12
Jay Fong <fjc0kb@gmail.com>
2020-06-10 17:20:10 +0800

    move

--- src/utils/swap.test.ts
+++ src/utils/swap.test.ts
@@ -4,5 +4,6 @@ describe('swap', () => {
   test('是原地交换且交换正常', () => {
     const arr = [1, 2, 3]
     expect(swap(arr, 0, 2)).toBe(arr).toEqual([3, 2, 1])
+    expect(swap(arr, 1, 1)).toBe(arr).toEqual([3, 2, 1])
   })
 })
