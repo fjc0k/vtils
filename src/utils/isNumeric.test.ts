0a405c344ba26cc2dc8f803c32a94adacb981075
Jay Fong <fjc0kb@gmail.com>
2020-06-10 18:29:53 +0800

    isNumeric

---
+++ src/utils/isNumeric.test.ts
@@ -0,0 +1,26 @@
+import { isNumeric } from './isNumeric'
+
+describe('isNumeric', () => {
+  test('不是数值', () => {
+    for (const value of [
+      '',
+      false,
+      null,
+      undefined,
+      () => 1,
+      [],
+      {},
+      NaN,
+      Infinity,
+      Number.NEGATIVE_INFINITY,
+    ]) {
+      expect(isNumeric(value)).toBeFalse()
+    }
+  })
+
+  test('是数值', () => {
+    for (const value of ['1', '566', '-22', 1.2, 2, -0.45, 0]) {
+      expect(isNumeric(value)).toBeTrue()
+    }
+  })
+})
