cb342b2ac297d1ab38a9209f081edc4952347017
Jay Fong <fjc0kb@gmail.com>
2020-06-11 10:48:43 +0800

    up

--- scripts/fixTypesBuild.ts
+++ scripts/fixTypesBuild.ts
@@ -1,12 +1,13 @@
+import fs from 'fs-extra'
 import globby from 'globby'
 import rollupPluginDts from 'rollup-plugin-dts'
 import { join } from 'path'
-import { remove } from 'fs-extra'
 import { rollup } from 'rollup'
 
 async function main(rootDir: string) {
   const typesDir = join(rootDir, './lib/types')
   const typesFile = join(typesDir, './index.d.ts')
+  const indexFile = join(typesDir, './index.js')
   const bundle = await rollup({
     input: typesFile,
     treeshake: true,
@@ -20,11 +21,20 @@ async function main(rootDir: string) {
     file: typesFile,
     format: 'es',
   })
-  const files = await globby(['*', '!index.d.ts'], {
+  const files = await globby(['*', '!index.js', '!index.d.ts'], {
     cwd: typesDir,
     absolute: true,
   })
-  await Promise.all(files.map(file => remove(file)))
+  await Promise.all(files.map(file => fs.remove(file)))
+  const [indexContent, typesContent] = await Promise.all([
+    fs.readFile(indexFile, 'utf8'),
+    fs.readFile(typesFile, 'utf8'),
+  ])
+  const indexDocs = indexContent.split(/(?<=\*\/)/)[0]
+  await Promise.all([
+    fs.writeFile(typesFile, `${indexDocs}\n${typesContent}`),
+    fs.writeFile(indexFile, 'export {}'),
+  ])
 }
 
 main(join(__dirname, '..'))
