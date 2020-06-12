eea03dccd6ea1c2aaebc7b9cb41be378622b4c19
Jay Fong <fjc0kb@gmail.com>
2020-06-12 23:52:04 +0800

    up

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,3 +1,9 @@
+/**
+ * 验证工具库。基于 {@link https://github.com/jquense/yup#readme | yup}。
+ *
+ * @packageDocumentation
+ */
+
 // @index(['./**/*.ts', '!**/*.test.*', '!./yupTypes.ts'], f => `export * from '${f.path}'`)
 export * from './locale/enUS'
 export * from './locale/zhCN'
