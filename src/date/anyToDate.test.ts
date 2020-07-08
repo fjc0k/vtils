665d918ebbc5f6e3d4aedde6d9bb17636bec7191
Jay Fong <fjc0kb@gmail.com>
2020-07-08 14:59:10 +0800

    test: anyToDate 测试修复

--- src/date/anyToDate.test.ts
+++ src/date/anyToDate.test.ts
@@ -6,20 +6,12 @@ describe('anyToDate', () => {
 
     expect(anyToDate(now.getTime())).toEqual(now)
     expect(anyToDate(Math.round(now.getTime() / 1000))).toEqual(
-      (() => {
-        const _now = new Date(now)
-        _now.setMilliseconds(0)
-        return _now
-      })(),
+      new Date(Math.round(now.getTime() / 1000) * 1000),
     )
 
     expect(anyToDate(String(now.getTime()))).toEqual(now)
     expect(anyToDate(String(Math.round(now.getTime() / 1000)))).toEqual(
-      (() => {
-        const _now = new Date(now)
-        _now.setMilliseconds(0)
-        return _now
-      })(),
+      new Date(Math.round(now.getTime() / 1000) * 1000),
     )
 
     expect(anyToDate(now.toISOString())).toEqual(now)
