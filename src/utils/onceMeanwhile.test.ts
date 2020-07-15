bfa244e3ea20e38ecc978b0686b556e24afc9a06
Jay Fong <fjc0kb@gmail.com>
2020-07-15 17:35:45 +0800

    feat(utils): 新增 onceMeanwhile

---
+++ src/utils/onceMeanwhile.test.ts
@@ -0,0 +1,20 @@
+import { onceMeanwhile } from './onceMeanwhile'
+import { wait } from './wait'
+
+describe('onceMeanwhile', () => {
+  test('表现正常', async () => {
+    const fn: () => Promise<1> = jest.fn().mockImplementation(async () => {
+      await wait(50)
+      return 1
+    })
+    const fn$ = onceMeanwhile(fn)
+
+    const res = await Promise.all([fn$(), fn$(), fn$(), fn$()])
+    expect(res).toEqual([1, 1, 1, 1])
+    expect(fn).toBeCalled().toBeCalledTimes(1)
+
+    const r = await fn$()
+    expect(r).toEqual(1)
+    expect(fn).toBeCalled().toBeCalledTimes(2)
+  })
+})
---
+++ src/utils/onceMeanwhile.ts
@@ -0,0 +1,29 @@
+import { AnyAsyncFunction } from '../types'
+import { isPromiseLike } from './isPromiseLike'
+
+/**
+ * 同一时间对函数的调用只会触发一次运行。
+ *
+ * @param fn 函数
+ * @returns 返回函数调用结果
+ */
+export function onceMeanwhile<TFunc extends AnyAsyncFunction>(
+  fn: TFunc,
+): TFunc {
+  let running = false
+  let result: Promise<any>
+  const proxy = (...args: any[]) => {
+    if (!running) {
+      running = true
+      const res = fn(...args)
+      if (isPromiseLike(res)) {
+        result = res.then(_ => {
+          running = false
+          return _
+        })
+      }
+    }
+    return result
+  }
+  return proxy as any
+}
