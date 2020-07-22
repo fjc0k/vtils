8d45de0db007baf468ac296a2b5c27059d86a2b2
Jay Fong <fjc0kb@gmail.com>
2020-07-22 13:01:24 +0800

    fix(useInterval): 修正测试用例

--- src/react/useInterval.test.ts
+++ src/react/useInterval.test.ts
@@ -6,7 +6,13 @@ import { wait } from '../utils'
 describe('useInterval', () => {
   test('通过 stop/start 控制', async () => {
     let i = 0
-    const { result } = renderHook(() => useInterval(() => i++, 20))
+    const { result } = renderHook(() =>
+      useInterval(() => {
+        const prevI = i
+        i = i + 1
+        return prevI
+      }, 20),
+    )
     expect(result.current[0]).toBe(0)
     await wait(20)
     expect(result.current[0]).toBe(1)
@@ -17,13 +23,21 @@ describe('useInterval', () => {
     expect(result.current[0]).toBe(2)
     await wait(25)
     expect(result.current[0]).toBe(3)
+    result.current[1].stop()
   })
 
   test('通过 delay 控制', async () => {
     let i = 0
     const { result } = renderHook(() => {
       const [delay, setDelay] = useState<any>(20)
-      return { interval: useInterval(() => i++, delay), setDelay }
+      return {
+        interval: useInterval(() => {
+          const prevI = i
+          i = i + 1
+          return prevI
+        }, delay),
+        setDelay: setDelay,
+      }
     })
     expect(result.current.interval[0]).toBe(0)
     await wait(25)
@@ -36,21 +50,30 @@ describe('useInterval', () => {
     expect(result.current.interval[0]).toBe(2)
     await wait(25)
     expect(result.current.interval[0]).toBe(3)
+    result.current.interval[1].stop()
   })
 
   test('支持 duration', async () => {
     let i = 0
-    renderHook(() => {
-      return useInterval(() => i++, 10, 21)
-    })
+    renderHook(() =>
+      useInterval(
+        () => {
+          i = i + 1
+        },
+        10,
+        28,
+      ),
+    )
     await wait(50)
-    expect(i).toBe(2)
+    expect(i).toBe(3)
   })
 
   test('支持 start 设置 delay, duration', async () => {
     let i = 0
     const { result } = renderHook(() => {
-      return useInterval(() => i++, null)[1]
+      return useInterval(() => {
+        i = i + 1
+      }, null)[1]
     })
     expect(i).toBe(0)
     act(() => result.current.start(10, 3))
