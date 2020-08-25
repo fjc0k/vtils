f102307fbce31084ff3ec092783ba42a96424d79
Jay Fong <fjc0kb@gmail.com>
2020-08-25 23:14:54 +0800

    fix(navigatePageTo): 兼容支付宝小程序

--- src/mp/navigatePageTo.ts
+++ src/mp/navigatePageTo.ts
@@ -16,32 +16,40 @@ export function navigatePageTo(
   redirect?: boolean,
 ): Promise<any> {
   return ensureInMiniProgram(mp => {
-    return new Promise((resolve, reject) => {
-      if (isUrl(url)) {
-        const { webUrlToMiniProgramUrl } = getMiniProgramConfig()
-        if (typeof webUrlToMiniProgramUrl === 'function') {
-          url = webUrlToMiniProgramUrl(url)
-        }
+    if (isUrl(url)) {
+      const { webUrlToMiniProgramUrl } = getMiniProgramConfig()
+      if (typeof webUrlToMiniProgramUrl === 'function') {
+        url = webUrlToMiniProgramUrl(url)
       }
-      if (query && typeof query === 'object') {
-        const queryString = createUrlQueryString(query)
-        if (queryString) {
-          url += (url.indexOf('?') > -1 ? '&' : '?') + queryString
-        }
+    }
+    if (query && typeof query === 'object') {
+      const queryString = createUrlQueryString(query)
+      if (queryString) {
+        url += (url.indexOf('?') > -1 ? '&' : '?') + queryString
       }
-      ;(redirect
-        ? ((mp.redirectTo as any) as typeof mp.navigateTo)
-        : mp.navigateTo)({
-        url: url,
-        success: resolve,
-        fail: () => {
-          mp.switchTab({
-            url: url,
-            success: resolve,
-            fail: reject,
-          })
-        },
+    }
+    const switchTab = () =>
+      new Promise((resolve, reject) => {
+        mp.switchTab({
+          url: url,
+          success: resolve,
+          fail: reject,
+        })
       })
-    })
+    const navigateTo = () =>
+      new Promise((resolve, reject) => {
+        ;(redirect
+          ? ((mp.redirectTo as any) as typeof mp.navigateTo)
+          : mp.navigateTo)({
+          url: url,
+          success: resolve,
+          fail: reject,
+        })
+      })
+    // 支付宝下 navigateTo、redirectTo 也能跳转到 Tab 页并不会报错，只是不会显示 Tab 栏
+    if (mp.$brand === '支付宝') {
+      return switchTab().catch(navigateTo)
+    }
+    return navigateTo().catch(switchTab)
   })
 }
