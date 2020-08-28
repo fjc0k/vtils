c46cacdae094b0218e6758c8886724cfdab195d4
Jay Fong <fjc0kb@gmail.com>
2020-08-28 16:02:10 +0800

    fix(getCurrentPageQuery): 对值执行 decodeURIComponent

--- src/mp/getCurrentPageQuery.ts
+++ src/mp/getCurrentPageQuery.ts
@@ -1,15 +1,21 @@
 import { ensureInMiniProgram } from './ensureInMiniProgram'
+import { mapValues } from '../utils'
 
 /**
- * 获取当前页面的查询参数。
+ * 获取当前页面的查询参数，已经对每个值执行了 decodeURIComponent。
  *
  * @returns 返回当前页面的查询参数
  */
-export function getCurrentPageQuery<T extends Record<string, string>>(): T {
+export function getCurrentPageQuery<
+  T extends Record<string, string | undefined>
+>(): T {
   return ensureInMiniProgram(() => {
     const pages = getCurrentPages()
     const currentPage = pages[pages.length - 1]
-    const query = currentPage.options || {}
+    const query = mapValues(
+      currentPage.options || {},
+      value => value && decodeURIComponent(value),
+    )
     return (query as any) as T
   })
 }
