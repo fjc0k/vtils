dc913211d095ae7ba6cb7a3e52f88ec023b16f2b
Jay Fong <fjc0kb@gmail.com>
2020-11-23 15:28:06 +0800

    fix: 修复 commonjs 配置

--- scripts/generatePackage.ts
+++ scripts/generatePackage.ts
@@ -2,10 +2,12 @@ import fs from 'fs-extra'
 import rawPackageInfo from '../package.json'
 import { join } from 'path'
 import { omit } from 'lodash-uni'
+import { PackageJson } from '../src/types'
 
 async function main(rootDir: string) {
   const packageDir = join(rootDir, './lib')
   const packageJsonFile = join(packageDir, './package.json')
+  const cjsPackageJsonFile = join(packageDir, './_cjs/package.json')
 
   const packageInfo = omit(rawPackageInfo, ([
     'husky',
@@ -18,6 +20,13 @@ async function main(rootDir: string) {
 
   await Promise.all([
     fs.writeJSON(packageJsonFile, packageInfo, { spaces: 2 }),
+    fs.outputJSON(
+      cjsPackageJsonFile,
+      {
+        type: 'commonjs',
+      } as PackageJson,
+      { spaces: 2 },
+    ),
     ...['LICENSE', 'README.md'].map(file => {
       return fs.copyFile(join(rootDir, file), join(packageDir, file))
     }),
