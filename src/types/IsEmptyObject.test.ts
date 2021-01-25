c056fd9dbf343aea9efed81335b08ca5b15ad4ca
Jay Fong <fjc0kb@gmail.com>
2021-01-25 11:20:32 +0800

    feat(types): 新增 IsEmptyObject

---
+++ src/types/IsEmptyObject.test.ts
@@ -0,0 +1,19 @@
+import { expectType } from '../dev'
+import { IsEmptyObject } from './IsEmptyObject'
+
+describe('IsEmptyObject', () => {
+  test('不是空对象', () => {
+    expectType<IsEmptyObject<number>, false>()
+    expectType<IsEmptyObject<boolean>, false>()
+    expectType<IsEmptyObject<string>, false>()
+    expectType<IsEmptyObject<RegExp>, false>()
+    expectType<IsEmptyObject<undefined>, false>()
+    expectType<IsEmptyObject<null>, false>()
+    expectType<IsEmptyObject<unknown>, false>()
+    expectType<IsEmptyObject<any[]>, false>()
+  })
+
+  test('是空对象', () => {
+    expectType<IsEmptyObject<{}>, true>()
+  })
+})
---
+++ src/types/IsEmptyObject.ts
@@ -0,0 +1,13 @@
+import { IsNever } from './IsNever'
+
+/**
+ * 判断 `T` 是否是空对象。
+ *
+ * @public
+ * @example
+ * ```typescript
+ * type X = IsEmptyObject<{}>
+ * // => true
+ * ```
+ */
+export type IsEmptyObject<T> = T extends Object ? IsNever<keyof T> : false
