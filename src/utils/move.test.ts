27eaf9771e97726d55ddf28678027c5fce73e41f
Jay Fong <fjc0kb@gmail.com>
2021-09-02 11:33:07 +0800

    feat(utils): 新增 moveUp, moveDown, moveToTop, moveToBottom

--- src/utils/move.test.ts
+++ src/utils/move.test.ts
@@ -1,4 +1,4 @@
-import { move } from './move'
+import { move, moveDown, moveToBottom, moveToTop, moveUp } from './move'
 
 describe('move', () => {
   test('是原地移动且移动正常', () => {
@@ -21,5 +21,63 @@ describe('move', () => {
     const arr: any[] = []
     expect(move(arr.slice(), 0, 1)).toEqual([])
     expect(move(arr.slice(), 1, 1)).toEqual([])
+  })
+
+  test('移动至负数位置正常', () => {
+    const arr: any[] = [1, 2, 3]
+    expect(move(arr.slice(), 0, -1)).toEqual([1, 2, 3])
+    expect(move(arr.slice(), 0, -2)).toEqual([1, 2, 3])
+    expect(move(arr.slice(), 1, -1)).toEqual([2, 1, 3])
+    expect(move(arr.slice(), 1, -200)).toEqual([2, 1, 3])
+    expect(move(arr.slice(), 5, -1)).toEqual([1, 2, 3])
+    expect(move(arr.slice(), 5, -100)).toEqual([1, 2, 3])
+  })
+
+  test('moveUp 正常', () => {
+    const arr: any[] = [1, 2, 3]
+    expect(moveUp(arr.slice(), -1)).toEqual([1, 2, 3])
+    expect(moveUp(arr.slice(), 0)).toEqual([1, 2, 3])
+    expect(moveUp(arr.slice(), 1)).toEqual([2, 1, 3])
+    expect(moveUp(arr.slice(), 2)).toEqual([1, 3, 2])
+    expect(moveUp(arr.slice(), 3)).toEqual([1, 2, 3])
+
+    expect(moveUp(arr.slice(), -1, 2)).toEqual([1, 2, 3])
+    expect(moveUp(arr.slice(), 0, 2)).toEqual([1, 2, 3])
+    expect(moveUp(arr.slice(), 1, 2)).toEqual([2, 1, 3])
+    expect(moveUp(arr.slice(), 2, 2)).toEqual([3, 1, 2])
+    expect(moveUp(arr.slice(), 3, 2)).toEqual([1, 2, 3])
+  })
+
+  test('moveDown 正常', () => {
+    const arr: any[] = [1, 2, 3]
+    expect(moveDown(arr.slice(), -1)).toEqual([1, 2, 3])
+    expect(moveDown(arr.slice(), 0)).toEqual([2, 1, 3])
+    expect(moveDown(arr.slice(), 1)).toEqual([1, 3, 2])
+    expect(moveDown(arr.slice(), 2)).toEqual([1, 2, 3])
+    expect(moveDown(arr.slice(), 3)).toEqual([1, 2, 3])
+
+    expect(moveDown(arr.slice(), -1, 2)).toEqual([1, 2, 3])
+    expect(moveDown(arr.slice(), 0, 2)).toEqual([2, 3, 1])
+    expect(moveDown(arr.slice(), 1, 2)).toEqual([1, 3, 2])
+    expect(moveDown(arr.slice(), 2, 2)).toEqual([1, 2, 3])
+    expect(moveDown(arr.slice(), 3, 2)).toEqual([1, 2, 3])
+  })
+
+  test('moveToTop 正常', () => {
+    const arr: any[] = [1, 2, 3]
+    expect(moveToTop(arr.slice(), -1)).toEqual([1, 2, 3])
+    expect(moveToTop(arr.slice(), 0)).toEqual([1, 2, 3])
+    expect(moveToTop(arr.slice(), 1)).toEqual([2, 1, 3])
+    expect(moveToTop(arr.slice(), 2)).toEqual([3, 1, 2])
+    expect(moveToTop(arr.slice(), 3)).toEqual([1, 2, 3])
+  })
+
+  test('moveToBottom 正常', () => {
+    const arr: any[] = [1, 2, 3]
+    expect(moveToBottom(arr.slice(), -1)).toEqual([1, 2, 3])
+    expect(moveToBottom(arr.slice(), 0)).toEqual([2, 3, 1])
+    expect(moveToBottom(arr.slice(), 1)).toEqual([1, 3, 2])
+    expect(moveToBottom(arr.slice(), 2)).toEqual([1, 2, 3])
+    expect(moveToBottom(arr.slice(), 3)).toEqual([1, 2, 3])
   })
 })
--- src/utils/move.ts
+++ src/utils/move.ts
@@ -11,8 +11,75 @@
  * ```
  */
 export function move<T>(arr: T[], from: number, to: number): T[] {
-  if (arr.length === 0 || from >= arr.length || from === to) return arr
+  const total = arr.length
+  if (total === 0 || from < 0 || from >= total) return arr
+  to = Math.min(total - 1, Math.max(0, to))
+  if (from === to) return arr
   const item = arr.splice(from, 1)[0]
   arr.splice(to, 0, item)
   return arr
+}
+
+/**
+ * 原地上移数组中的某个元素。
+ *
+ * @param arr 要处理的数组
+ * @param from 要移动元素的索引
+ * @param step 移动步值
+ * @returns 返回移动后的数组
+ * @example
+ * ```typescript
+ * moveUp([1, 2, 3], 2) // => [1, 3, 2]
+ * moveUp([1, 2, 3], 2, 2) // => [3, 1, 2]
+ * ```
+ */
+export function moveUp<T>(arr: T[], from: number, step = 1): T[] {
+  return move(arr, from, from - step)
+}
+
+/**
+ * 原地下移数组中的某个元素。
+ *
+ * @param arr 要处理的数组
+ * @param from 要移动元素的索引
+ * @param step 移动步值
+ * @returns 返回移动后的数组
+ * @example
+ * ```typescript
+ * moveDown([1, 2, 3], 0) // => [2, 1, 3]
+ * moveDown([1, 2, 3], 0, 2) // => [2, 3, 1]
+ * ```
+ */
+export function moveDown<T>(arr: T[], from: number, step = 1): T[] {
+  return move(arr, from, from + step)
+}
+
+/**
+ * 原地置顶数组中的某个元素。
+ *
+ * @param arr 要处理的数组
+ * @param from 要移动元素的索引
+ * @returns 返回移动后的数组
+ * @example
+ * ```typescript
+ * moveToTop([1, 2, 3], 2) // => [3, 1, 2]
+ * ```
+ */
+export function moveToTop<T>(arr: T[], from: number): T[] {
+  return move(arr, from, 0)
+}
+
+/**
+ * 原地置底数组中的某个元素。
+ *
+ * @param arr 要处理的数组
+ * @param from 要移动元素的索引
+ * @returns 返回移动后的数组
+ * @example
+ * ```typescript
+ * moveToBottom([1, 2, 3], 0) // => [2, 3, 1]
+ * ```
+ */
+export function moveToBottom<T>(arr: T[], from: number): T[] {
+  return move(arr, from, arr.length - 1)
 }
