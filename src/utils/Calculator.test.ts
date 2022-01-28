87c754c6b9d7e8acc140d666f17ca7de694b1ec1
Jay Fong <fjc0kb@gmail.com>
2022-01-28 11:48:18 +0800

    chore(Calculator): 增加测试用例

--- src/utils/Calculator.test.ts
+++ src/utils/Calculator.test.ts
@@ -1,38 +1,78 @@
 import { Calculator } from './Calculator'
 
 describe('Calculator', () => {
-  test('加正常', () => {
-    expect(0.1 + 0.2).not.toBe(0.3)
-    expect(Calculator.add(0.1, 0.2)).toBe(0.3)
+  describe('加', () => {
+    test('加数列表为空时返回0', () => {
+      expect(Calculator.add()).toBe(0)
+    })
 
-    expect(0.1 + 0.7).not.toBe(0.8)
-    expect(Calculator.add(0.1, 0.7)).toBe(0.8)
+    test('加数列表长度为1时返回第1个的值', () => {
+      expect(Calculator.add(2.2)).toBe(2.2)
+    })
 
-    expect(0.2 + 0.4).not.toBe(0.6)
-    expect(Calculator.add(0.2, 0.4)).toBe(0.6)
+    test('加数列表长度大于2时返回相加结果', () => {
+      expect(0.1 + 0.2).not.toBe(0.3)
+      expect(Calculator.add(0.1, 0.2)).toBe(0.3)
+
+      expect(0.1 + 0.7).not.toBe(0.8)
+      expect(Calculator.add(0.1, 0.7)).toBe(0.8)
+
+      expect(0.2 + 0.4).not.toBe(0.6)
+      expect(Calculator.add(0.2, 0.4)).toBe(0.6)
+    })
   })
 
-  test('减正常', () => {
-    expect(1 - 0.9).not.toBe(0.1)
-    expect(Calculator.sub(1, 0.9)).toBe(0.1)
+  describe('减', () => {
+    test('减数列表为空时返回0', () => {
+      expect(Calculator.sub()).toBe(0)
+    })
 
-    expect(1.5 - 1.2).not.toBe(0.3)
-    expect(Calculator.sub(1.5, 1.2)).toBe(0.3)
+    test('减数列表长度为1时返回第1个的值', () => {
+      expect(Calculator.sub(2.2)).toBe(2.2)
+    })
+
+    test('减数列表长度大于2时返回相减结果', () => {
+      expect(1 - 0.9).not.toBe(0.1)
+      expect(Calculator.sub(1, 0.9)).toBe(0.1)
+
+      expect(1.5 - 1.2).not.toBe(0.3)
+      expect(Calculator.sub(1.5, 1.2)).toBe(0.3)
+    })
   })
 
-  test('乘正常', () => {
-    expect(0.8 * 3).not.toBe(2.4)
-    expect(Calculator.mul(0.8, 3)).toBe(2.4)
+  describe('乘', () => {
+    test('乘数列表为空时返回0', () => {
+      expect(Calculator.mul()).toBe(0)
+    })
 
-    expect(0.55 * 100).not.toBe(55)
-    expect(Calculator.mul(0.55, 100)).toBe(55)
+    test('乘数列表长度为1时返回第1个的值', () => {
+      expect(Calculator.mul(2.2)).toBe(2.2)
+    })
+
+    test('乘数列表长度大于2时返回相乘结果', () => {
+      expect(0.8 * 3).not.toBe(2.4)
+      expect(Calculator.mul(0.8, 3)).toBe(2.4)
+
+      expect(0.55 * 100).not.toBe(55)
+      expect(Calculator.mul(0.55, 100)).toBe(55)
+    })
   })
 
-  test('除正常', () => {
-    expect(0.3 / 0.1).not.toBe(3)
-    expect(Calculator.div(0.3, 0.1)).toBe(3)
+  describe('除', () => {
+    test('除数列表为空时返回0', () => {
+      expect(Calculator.div()).toBe(0)
+    })
 
-    expect(0.69 / 10).not.toBe(0.069)
-    expect(Calculator.div(0.69, 10)).toBe(0.069)
+    test('除数列表长度为1时返回第1个的值', () => {
+      expect(Calculator.div(2.2)).toBe(2.2)
+    })
+
+    test('除数列表长度大于2时返回相除结果', () => {
+      expect(0.3 / 0.1).not.toBe(3)
+      expect(Calculator.div(0.3, 0.1)).toBe(3)
+
+      expect(0.69 / 10).not.toBe(0.069)
+      expect(Calculator.div(0.69, 10)).toBe(0.069)
+    })
   })
 })
