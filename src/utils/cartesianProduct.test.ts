fee9b140809bca947ae977231ed921f214c9b5dd
Jay Fong <fjc0kb@gmail.com>
2022-01-29 14:32:30 +0800

    chore: cartesianProduct test

--- src/utils/cartesianProduct.test.ts
+++ src/utils/cartesianProduct.test.ts
@@ -1,207 +1,209 @@
 const _Function = Function
 
-describe('cartesianProduct - fast', () => {
-  beforeAll(() => {
-    jest.resetModules()
-    global.Function = _Function
+describe('cartesianProduct', () => {
+  describe('fast', () => {
+    beforeAll(() => {
+      jest.resetModules()
+      global.Function = _Function
+    })
+
+    test('正常', async () => {
+      const { cartesianProduct } = await import('./cartesianProduct')
+
+      expect(cartesianProduct([])).toEqual([])
+
+      expect(cartesianProduct([[], []])).toEqual([])
+
+      expect(cartesianProduct([['a']])).toEqual([['a']])
+
+      expect(cartesianProduct([['a', 'b']])).toEqual([['a'], ['b']])
+
+      expect(
+        cartesianProduct([
+          ['a', 'b'],
+          [1, 2],
+        ]),
+      ).toEqual([
+        ['a', 1],
+        ['a', 2],
+        ['b', 1],
+        ['b', 2],
+      ])
+
+      // adding a 0 element set causes the entire product to be empty.
+      expect(cartesianProduct([['a', 'b'], [1, 2], []])).toEqual([])
+
+      expect(
+        cartesianProduct([
+          [1, 2],
+          ['a', 'b', 'c'],
+        ]),
+      ).toEqual([
+        [1, 'a'],
+        [1, 'b'],
+        [1, 'c'],
+        [2, 'a'],
+        [2, 'b'],
+        [2, 'c'],
+      ])
+
+      expect(
+        cartesianProduct([
+          ['a', 'b'],
+          [1, 2],
+          [[], {}],
+        ]),
+      ).toEqual([
+        ['a', 1, []],
+        ['a', 1, {}],
+        ['a', 2, []],
+        ['a', 2, {}],
+        ['b', 1, []],
+        ['b', 1, {}],
+        ['b', 2, []],
+        ['b', 2, {}],
+      ])
+    })
+
+    test('报错', async () => {
+      const { cartesianProduct } = await import('./cartesianProduct')
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(),
+      ).toThrowError(/expects an array/)
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(1),
+      ).toThrowError(/expects an array/)
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(null),
+      ).toThrowError(/expects an array/)
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct('js'),
+      ).toThrowError(/expects an array/)
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(['dd', ['d']]),
+      ).toThrowError(/index 0 must be an array/)
+
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct([['dd'], ['d'], {}]),
+      ).toThrowError(/index 2 must be an array/)
+    })
   })
 
-  test('正常', async () => {
-    const { cartesianProduct } = await import('./cartesianProduct')
-
-    expect(cartesianProduct([])).toEqual([])
-
-    expect(cartesianProduct([[], []])).toEqual([])
-
-    expect(cartesianProduct([['a']])).toEqual([['a']])
-
-    expect(cartesianProduct([['a', 'b']])).toEqual([['a'], ['b']])
-
-    expect(
-      cartesianProduct([
-        ['a', 'b'],
-        [1, 2],
-      ]),
-    ).toEqual([
-      ['a', 1],
-      ['a', 2],
-      ['b', 1],
-      ['b', 2],
-    ])
-
-    // adding a 0 element set causes the entire product to be empty.
-    expect(cartesianProduct([['a', 'b'], [1, 2], []])).toEqual([])
-
-    expect(
-      cartesianProduct([
-        [1, 2],
-        ['a', 'b', 'c'],
-      ]),
-    ).toEqual([
-      [1, 'a'],
-      [1, 'b'],
-      [1, 'c'],
-      [2, 'a'],
-      [2, 'b'],
-      [2, 'c'],
-    ])
-
-    expect(
-      cartesianProduct([
-        ['a', 'b'],
-        [1, 2],
-        [[], {}],
-      ]),
-    ).toEqual([
-      ['a', 1, []],
-      ['a', 1, {}],
-      ['a', 2, []],
-      ['a', 2, {}],
-      ['b', 1, []],
-      ['b', 1, {}],
-      ['b', 2, []],
-      ['b', 2, {}],
-    ])
-  })
-
-  test('报错', async () => {
-    const { cartesianProduct } = await import('./cartesianProduct')
-
-    expect(() =>
+  describe('universal', () => {
+    beforeAll(() => {
+      jest.resetModules()
       // @ts-expect-error
-      cartesianProduct(),
-    ).toThrowError(/expects an array/)
-
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(1),
-    ).toThrowError(/expects an array/)
-
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(null),
-    ).toThrowError(/expects an array/)
-
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct('js'),
-    ).toThrowError(/expects an array/)
-
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(['dd', ['d']]),
-    ).toThrowError(/index 0 must be an array/)
-
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct([['dd'], ['d'], {}]),
-    ).toThrowError(/index 2 must be an array/)
-  })
-})
-
-describe('cartesianProduct - universal', () => {
-  beforeAll(() => {
-    jest.resetModules()
-    // @ts-expect-error
-    global.Function = class Function2 extends _Function {
-      constructor(...args: any[]) {
-        if (args[0] === 'return 1') {
-          throw new Error()
+      global.Function = class Function2 extends _Function {
+        constructor(...args: any[]) {
+          if (args[0] === 'return 1') {
+            throw new Error()
+          }
+          super(...args)
         }
-        super(...args)
       }
-    }
-  })
+    })
 
-  test('正常', async () => {
-    const { cartesianProduct } = await import('./cartesianProduct')
+    test('正常', async () => {
+      const { cartesianProduct } = await import('./cartesianProduct')
 
-    expect(cartesianProduct([])).toEqual([])
+      expect(cartesianProduct([])).toEqual([])
 
-    expect(cartesianProduct([[], []])).toEqual([])
+      expect(cartesianProduct([[], []])).toEqual([])
 
-    expect(cartesianProduct([['a']])).toEqual([['a']])
+      expect(cartesianProduct([['a']])).toEqual([['a']])
 
-    expect(cartesianProduct([['a', 'b']])).toEqual([['a'], ['b']])
+      expect(cartesianProduct([['a', 'b']])).toEqual([['a'], ['b']])
 
-    expect(
-      cartesianProduct([
-        ['a', 'b'],
-        [1, 2],
-      ]),
-    ).toEqual([
-      ['a', 1],
-      ['a', 2],
-      ['b', 1],
-      ['b', 2],
-    ])
+      expect(
+        cartesianProduct([
+          ['a', 'b'],
+          [1, 2],
+        ]),
+      ).toEqual([
+        ['a', 1],
+        ['a', 2],
+        ['b', 1],
+        ['b', 2],
+      ])
 
-    // adding a 0 element set causes the entire product to be empty.
-    expect(cartesianProduct([['a', 'b'], [1, 2], []])).toEqual([])
+      // adding a 0 element set causes the entire product to be empty.
+      expect(cartesianProduct([['a', 'b'], [1, 2], []])).toEqual([])
 
-    expect(
-      cartesianProduct([
-        [1, 2],
-        ['a', 'b', 'c'],
-      ]),
-    ).toEqual([
-      [1, 'a'],
-      [1, 'b'],
-      [1, 'c'],
-      [2, 'a'],
-      [2, 'b'],
-      [2, 'c'],
-    ])
+      expect(
+        cartesianProduct([
+          [1, 2],
+          ['a', 'b', 'c'],
+        ]),
+      ).toEqual([
+        [1, 'a'],
+        [1, 'b'],
+        [1, 'c'],
+        [2, 'a'],
+        [2, 'b'],
+        [2, 'c'],
+      ])
 
-    expect(
-      cartesianProduct([
-        ['a', 'b'],
-        [1, 2],
-        [[], {}],
-      ]),
-    ).toEqual([
-      ['a', 1, []],
-      ['a', 1, {}],
-      ['a', 2, []],
-      ['a', 2, {}],
-      ['b', 1, []],
-      ['b', 1, {}],
-      ['b', 2, []],
-      ['b', 2, {}],
-    ])
-  })
+      expect(
+        cartesianProduct([
+          ['a', 'b'],
+          [1, 2],
+          [[], {}],
+        ]),
+      ).toEqual([
+        ['a', 1, []],
+        ['a', 1, {}],
+        ['a', 2, []],
+        ['a', 2, {}],
+        ['b', 1, []],
+        ['b', 1, {}],
+        ['b', 2, []],
+        ['b', 2, {}],
+      ])
+    })
 
-  test('报错', async () => {
-    const { cartesianProduct } = await import('./cartesianProduct')
+    test('报错', async () => {
+      const { cartesianProduct } = await import('./cartesianProduct')
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(),
-    ).toThrowError(/expects an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(),
+      ).toThrowError(/expects an array/)
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(1),
-    ).toThrowError(/expects an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(1),
+      ).toThrowError(/expects an array/)
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(null),
-    ).toThrowError(/expects an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(null),
+      ).toThrowError(/expects an array/)
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct('js'),
-    ).toThrowError(/expects an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct('js'),
+      ).toThrowError(/expects an array/)
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct(['dd', ['d']]),
-    ).toThrowError(/index 0 must be an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct(['dd', ['d']]),
+      ).toThrowError(/index 0 must be an array/)
 
-    expect(() =>
-      // @ts-expect-error
-      cartesianProduct([['dd'], ['d'], {}]),
-    ).toThrowError(/index 2 must be an array/)
+      expect(() =>
+        // @ts-expect-error
+        cartesianProduct([['dd'], ['d'], {}]),
+      ).toThrowError(/index 2 must be an array/)
+    })
   })
 })
