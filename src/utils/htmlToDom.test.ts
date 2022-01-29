fd5bdfd13218b805370d5107cf7b97d1b5e36e5c
Jay Fong <fjc0kb@gmail.com>
2022-01-29 14:47:14 +0800

    chore: htmlToDocumentFragment test

--- src/utils/htmlToDom.test.ts
+++ src/utils/htmlToDom.test.ts
@@ -2,6 +2,10 @@ import { htmlToDocumentFragment, htmlToElement } from './htmlToDom'
 
 describe('htmlToDom', () => {
   test('htmlToDocumentFragment 正常', () => {
+    expect(
+      // @ts-expect-error
+      htmlToDocumentFragment(),
+    ).toMatchSnapshot()
     expect(htmlToDocumentFragment('test')).toMatchSnapshot()
     expect(htmlToDocumentFragment('test <a>hello</a>')).toMatchSnapshot()
     expect(htmlToDocumentFragment('<a>hello</a>')).toMatchSnapshot()
