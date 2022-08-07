8907a2993905235c5f654bf4f85fa5eb48bc28c0
Jay Fong <fjc0kb@gmail.com>
2022-08-07 20:38:41 +0800

    feat(utils): 新增 MiniProgramUrl

---
+++ src/utils/MiniProgramUrl.test.ts
@@ -0,0 +1,70 @@
+import { MiniProgramUrl } from './MiniProgramUrl'
+
+describe('MiniProgramUrl', () => {
+  test('ok', () => {
+    expect(
+      new MiniProgramUrl(
+        'mp://{"appId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ).toJson(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl(
+        'mp://{"appId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ).toString(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl(
+        'mp://{"appId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      )
+        .update({
+          path: '/pages/user?id=333',
+        })
+        .toJson(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl(
+        'mp://{"appId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ).toWxOpenLaunchWeappAttrs(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl(
+        'mp://{"rawId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ).toWxOpenLaunchWeappAttrs(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl({
+        rawId: 'testApp',
+        path: '/pages/home?from=223&se=33',
+      }).toWxOpenLaunchWeappAttrs(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl({
+        rawId: 'testApp',
+        path: '/pages/home?from=223&se=33',
+      }).toString(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl({
+        rawId: 'testApp',
+        path: 'pages/home?from=223&se=33',
+      }).toString(),
+    ).toMatchSnapshot()
+    expect(
+      new MiniProgramUrl({
+        appId: 'testApp',
+        path: '/pages/home?from=223&se=33',
+      }).toWxNavigateToMiniProgramParams(),
+    ).toMatchSnapshot()
+    expect(
+      MiniProgramUrl.is(
+        'mp://{"rawId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ),
+    ).toBeTrue()
+    expect(
+      MiniProgramUrl.is(
+        // @ts-expect-error
+        'https://{"rawId":"testApp","path":"/pages/index","query":{"x":"1"}}',
+      ),
+    ).toBeFalse()
+  })
+})
---
+++ src/utils/MiniProgramUrl.ts
@@ -0,0 +1,104 @@
+import { assign } from 'lodash-uni'
+import { createUrlQueryString } from './createUrlQueryString'
+import { parseUrlQueryString } from './parseUrlQueryString'
+
+export type MiniProgramUrlProvider = 'wechat'
+
+export type MiniProgramUrlVersion = 'release' | 'develop' | 'trial'
+
+export type MiniProgramUrlStringInput = `mp://${string}`
+
+export type MiniProgramUrlJsonInput = {
+  /** 提供商 */
+  provider?: MiniProgramUrlProvider
+  /** APPID */
+  appId?: string
+  /** 原始ID */
+  rawId?: string
+  /** 版本 */
+  version?: MiniProgramUrlVersion
+  /** 路径 */
+  path?: string
+  /** 查询参数 */
+  query?: Record<string, any>
+  /** 额外数据 */
+  data?: any
+}
+
+export type MiniProgramUrlInput =
+  | MiniProgramUrlStringInput
+  | MiniProgramUrlJsonInput
+
+/**
+ * 小程序链接。
+ */
+export class MiniProgramUrl {
+  private payload: MiniProgramUrlJsonInput = {}
+
+  constructor(payload: MiniProgramUrlInput) {
+    if (typeof payload === 'string') {
+      this.update(JSON.parse(payload.substring(5)))
+    } else {
+      this.update(payload)
+    }
+  }
+
+  private getPath() {
+    return (
+      (this.payload.path ? this.payload.path.substring(1) : '') +
+      (this.payload.query ? `?${createUrlQueryString(this.payload.query)}` : '')
+    )
+  }
+
+  update(payload: Partial<MiniProgramUrlJsonInput>): this {
+    if (payload.path) {
+      if (payload.path[0] !== '/') {
+        payload.path = `/${payload.path}`
+      }
+      const [path, query] = payload.path.split('?')
+      if (query) {
+        payload.path = path
+        payload.query = {
+          ...parseUrlQueryString(query),
+          ...payload.query,
+        }
+      }
+    }
+    assign(this.payload, payload)
+    return this
+  }
+
+  toJson(): MiniProgramUrlJsonInput {
+    return this.payload
+  }
+
+  toString(): string {
+    return `mp://${JSON.stringify(this.payload)}`
+  }
+
+  toWxOpenLaunchWeappAttrs() {
+    return {
+      username: this.payload.rawId,
+      path: this.getPath(),
+      envVersion: this.payload.version,
+      extraData: this.payload.data
+        ? JSON.stringify(this.payload.data)
+        : undefined,
+    }
+  }
+
+  toWxNavigateToMiniProgramParams() {
+    return {
+      appId: this.payload.appId,
+      path: this.getPath(),
+      envVersion: this.payload.version,
+      extraData: this.payload.data,
+    }
+  }
+
+  static is(
+    value: MiniProgramUrlStringInput,
+  ): value is MiniProgramUrlStringInput {
+    return value.substring(0, 5) === 'mp://'
+  }
+}
