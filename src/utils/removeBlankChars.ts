fa9cbbf548975b53f57c517f33a5f35e4e81018a
Jay Fong <fjc0kb@gmail.com>
2022-10-19 12:48:48 +0800

    fix(removeBlankChars): 新增 200E, 200F

--- src/utils/removeBlankChars.ts
+++ src/utils/removeBlankChars.ts
@@ -1,6 +1,7 @@
 // https://github.com/frandiox/normalize-unicode-text/blob/master/src/index.ts
+// 新增 200E, 200F
 const re =
-  /(\s+|[\u180E\u200B-\u200D\u2060\uFEFF]+|[ \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000]+|[\u2420\u2422\u2423]+)/gs
+  /(\s+|[\u180E\u200B-\u200F\u2060\uFEFF]+|[ \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000]+|[\u2420\u2422\u2423]+)/gs
 
 /**
  * 从字符串中移除空白字符。
