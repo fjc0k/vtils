e5f3363ccf14115a0c45759656eb14337af6fa6d
Jay Fong <fjc0kb@gmail.com>
2023-08-05 22:06:04 +0800

    VaeEnumSchema

--- src/vae/VaeEnumSchema.ts
+++ src/vae/VaeEnumSchema.ts
@@ -1,3 +1,17 @@
+import { includes, isArray, values } from 'lodash-uni'
 import { VaeBaseSchema } from './VaeBaseSchema'
+import { VaeLocale, VaeLocaleMessage } from './VaeLocale'
 
-export class VaeEnumSchema extends VaeBaseSchema<boolean> {}
+export class VaeEnumSchema<T extends any = any> extends VaeBaseSchema<T> {
+  constructor(
+    value: T[] | Record<any, T>,
+    message: VaeLocaleMessage = VaeLocale.enum.type,
+  ) {
+    super()
+    this.check({
+      fn: v =>
+        isArray(value) ? includes(value, v) : includes(values(value), v),
+      message: message,
+    })
+  }
+}
