56879e5edfbdb1848e888dd50771add2b9ff1a57
Jay Fong <fjc0kb@gmail.com>
2023-08-05 21:15:55 +0800

    VaeStringSchema

--- src/vae/VaeStringSchema.ts
+++ src/vae/VaeStringSchema.ts
@@ -1,7 +1,22 @@
+import { endsWith, includes, isString, startsWith } from 'lodash-uni'
+import {
+  isChineseIDCardNumber,
+  isEmail,
+  isPossibleChineseMobilePhoneNumber,
+  isUrl,
+} from '../utils'
 import { VaeBaseSchema } from './VaeBaseSchema'
 import { VaeLocale, VaeLocaleMessage } from './VaeLocale'
 
 export class VaeStringSchema extends VaeBaseSchema<string> {
+  constructor(message: VaeLocaleMessage = VaeLocale.string.type) {
+    super()
+    this.check({
+      fn: isString,
+      message: message,
+    })
+  }
+
   min(minLength: number, message: VaeLocaleMessage = VaeLocale.string.min) {
     return this.check({
       fn: v => v.length >= minLength,
@@ -9,22 +24,91 @@ export class VaeStringSchema extends VaeBaseSchema<string> {
     })
   }
 
-  // https://zod.dev/?id=strings
-  max() {}
-  length() {}
-  email() {}
-  url() {}
-  emoji() {}
-  uuid() {}
-  cuid() {}
-  cuid2() {}
-  regex() {}
-  includes() {}
-  startsWith() {}
-  endsWith() {}
-  datetime() {}
-  ip() {}
-  trim() {}
-  toLowerCase() {}
-  toUpperCase() {}
+  max(maxLength: number, message: VaeLocaleMessage = VaeLocale.string.max) {
+    return this.check({
+      fn: v => v.length <= maxLength,
+      message: message,
+    })
+  }
+
+  length(len: number, message: VaeLocaleMessage = VaeLocale.string.length) {
+    return this.check({
+      fn: v => v.length === len,
+      message: message,
+    })
+  }
+
+  email(message: VaeLocaleMessage = VaeLocale.string.email) {
+    return this.check({
+      fn: isEmail,
+      message: message,
+    })
+  }
+
+  url(message: VaeLocaleMessage = VaeLocale.string.url) {
+    return this.check({
+      fn: isUrl,
+      message: message,
+    })
+  }
+
+  regex(regex: RegExp, message: VaeLocaleMessage = VaeLocale.string.regex) {
+    return this.check({
+      fn: v => {
+        regex.lastIndex = 0
+        return regex.test(v)
+      },
+      message: message,
+    })
+  }
+
+  includes(
+    value: string,
+    message: VaeLocaleMessage = VaeLocale.string.includes,
+  ) {
+    return this.check({
+      fn: v => includes(v, value),
+      message: message,
+    })
+  }
+
+  startsWith(
+    value: string,
+    message: VaeLocaleMessage = VaeLocale.string.startsWith,
+  ) {
+    return this.check({
+      fn: v => startsWith(v, value),
+      message: message,
+    })
+  }
+
+  endsWith(
+    value: string,
+    message: VaeLocaleMessage = VaeLocale.string.endsWith,
+  ) {
+    return this.check({
+      fn: v => endsWith(v, value),
+      message: message,
+    })
+  }
+
+  chinesePhoneNumber(
+    value: string,
+    message: VaeLocaleMessage = VaeLocale.string.chinesePhoneNumber,
+  ) {
+    return this.check({
+      fn: isPossibleChineseMobilePhoneNumber,
+      message: message,
+    })
+  }
+
+  chineseIDCardNumber(
+    value: string,
+    message: VaeLocaleMessage = VaeLocale.string.chineseIDCardNumber,
+  ) {
+    return this.check({
+      fn: isChineseIDCardNumber,
+      message: message,
+    })
+  }
 }
